996cdf50f30de60f61ea03cf3702efba249b55e6
Luis Mountrose <welcomeluis@gmail.com>
2021-09-12 21:53:08 -0500

    nueva version

--- hello.py
+++ hello.py
@@ -1,4 +1,28 @@
-import myname
-
+"""import myname
 name = myname.get_name()
-print("hello world {}".format(name))
+print("hello world {}".format(name))"""
+
+import pymongo
+import pprint
+from IPython.display import clear_output
+
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://lmonterr:holamundo2021@cluster0.do2wc.mongodb.net/Cluster0?retryWrites=true&w=majority")
+print(client.mflix)
+
+pipeline = [
+    {
+        "$group": {
+            "_id": {"languaje": "$language"},
+            "count":{"$sum":1}
+        }
+    }
+]
+
+clear_output()
+
+
+pprint.pprint(list(client.Cluster0.movies_initial.aggregate(pipeline)))
+
+
+
